4932ea21a0dbda2d2d1cfda3c6cbda52b2495f1b
vsanjayathreya <Vivan.SanjayAthreya@logmein.com>
2022-03-06 12:14:57 +0530

    Updates

---
+++ precache-manifest.9035a141e7c65b98bf0c715e2b1bb592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fceabec4e50557a2a846",
+    "url": "/ToDo/static/js/runtime~main.fd0bb66b.js"
+  },
+  {
+    "revision": "b7545a5f0ffba1b879fd",
+    "url": "/ToDo/static/js/main.80142f5d.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/ToDo/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "44673e43ac128fc201ac",
+    "url": "/ToDo/static/js/2.b5a60ba2.chunk.js"
+  },
+  {
+    "revision": "b7545a5f0ffba1b879fd",
+    "url": "/ToDo/static/css/main.ea7cdb49.chunk.css"
+  },
+  {
+    "revision": "b9b66f9a59332862940f9b86f9838c63",
+    "url": "/ToDo/index.html"
+  }
+];
