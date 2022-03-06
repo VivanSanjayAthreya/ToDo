4c909c4989c8fc5a716241f0d1e8d2a3901a1d60
vsanjayathreya <Vivan.SanjayAthreya@logmein.com>
2022-03-06 12:44:28 +0530

    changed the home and about links in header

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
+                <Link className="navbar-brand" to="/ToDo/">
                     {props.title}
                 </Link>
                 <button
@@ -23,12 +23,12 @@ export default function Header(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">
+                            <Link className="nav-link active" aria-current="page" to="/ToDo/">
                                 Home
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">
+                            <Link className="nav-link" to="/ToDo/about">
                                 About
                             </Link>
                         </li>
